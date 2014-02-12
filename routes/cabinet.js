ea8a83dee5f0c9c518f5b6e2552df2d11c3565b3
Ranajay <ranajays@gmail.com>
2014-02-12 15:55:51 -0800

    Included Lato font for FlatUI as google font for now until rest of FlatUI stuff gets put in. Implemented basic user cabinet with default user (id=1). Updated cabinet to show only user's drinks.

--- routes/cabinet.js
+++ routes/cabinet.js
@@ -6,20 +6,29 @@
 exports.view = function(req, res){
 	var mongodb = require('mongodb');
 	var databaseUrl = "ranajays:ranajays@troup.mongohq.com:10078/app21902449"; // "username:password@example.com/mydb"
-	var collections = ["drinks", "ingredients", "recipes"];
+	var collections = ["drinks", "ingredients", "recipes", "cabinets"];
 	var db = require("mongojs").connect(databaseUrl, collections);
 	db.ingredients.find({}, {}, function(err,docs) {
 		if (err) {
 			console.log("error:" + err);
 		}
 		else {
-			var names =[];
+			var ingredients = [];
+			var ingredientMap = {};
 			for (var i = 0; i < docs.length; i++) {
-				names.push(docs[i].ingredient)
+				ingredients.push({label: docs[i].ingredient, id: docs[i].id});
+				ingredientMap[docs[i].id] = {ingredient: docs[i].ingredient, image_url: docs[i].image_url};
 			}
-			res.render('cabinet', {
-				docs: docs,
-				data: JSON.stringify(names)});
+			db.cabinets.find({user_id: 1}, {}, function (err,docs) {
+				console.log(docs);
+				cabinet = [];
+				for (var i = 0; i < docs.length; i++) {
+					cabinet.push(ingredientMap[docs[i].ingredient_id]);
+				}
+				res.render('cabinet', {
+					cabinet: cabinet,
+					ingredients: JSON.stringify(ingredients)});
+			});
 		}
 	});
 	// var $ = res.window.jQuery;
